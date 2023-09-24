13e6f02f193a6fb2b4acb20363de2bc0d9d18865
farazshams1998 <farazshams981@gmail.com>
2023-09-25 00:50:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password")
+alert("Welcome to JS land....\nHappy Coding!");
+
+var errorName = "Hello--I can run JS through my web browser's console"
+alert(errorName);
